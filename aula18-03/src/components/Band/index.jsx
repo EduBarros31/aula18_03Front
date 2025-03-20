484d363632688aa272627bfd7cd22a5d66fc8416
jsedubarros@gmail.com <{jsedubarros@gmail.com}>
2025-03-19 18:21:58 -0700

    ADD: criacao e utilizacao de routes em pro

--- aula18-03/src/components/Band/index.jsx
+++ aula18-03/src/components/Band/index.jsx
@@ -1,17 +1,36 @@
-import React from "react";
-import Style from "./Band.module.css"
+
+import Style from "./Band.module.css";
+import { React, useState }  from "react";
+
 
 function Band({imagem, nome, descricao}) {
+
+     const [like, setLike] = useState(0)
+     const [curiosidade, setCuriosidade] = useState()
+
+
     return (
         
+     
+
+
         <section className={Style.Band}>
         <div className="Band">
             <img src={imagem} alt={nome}></img>
             <div className="Bandinfo">
                 <h2>{nome}</h2>
                 <p>{descricao}</p>
+                <p>Curtidas: {like}</p>
+
             </div>
+            <button onClick={() =>setLike(like+1)}>Curtir </button>
+            <button onClick={() =>setDisLike( Dislike +1 && Dislike -1)}>Dislike</button>
+           
         </div>
+
+        
+
+
         </section>
     
 )
--- aula18-03/src/pages/BandDetalhes/BandDetalhes.jsx
+++ aula18-03/src/pages/BandDetalhes/BandDetalhes.jsx
@@ -2,20 +2,61 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import bandas from "../../data/bancoBandas";
 import Band from "../../components/Band/index"
+import { Link } from "react-router-dom"
+import Header from "../../components/Header"
+import Aside from "../../components/Aside"
+import Footer from "../../components/Footer"
+import styles from "./BandDetalhes.module.css"
 
 
 function BandDetalhes() {
     const { id } = useParams();
     const band = bandas.find((b) => b.id === parseInt(id));
 
-    if (!band) return <h2>Banda não encontrada</h2>
+    if (!band) 
+
+    
     return (
-        <>
-            <div>
-            <Band {...band} />
+      
+            <div classname={styles.container}>
+                <Header />
+                    <h2>
+                        Banda não encontrada
+                    </h2>
+                <Link to={`/`}>
+                  <button>Voltar</button></Link>
+                <Aside/>
+                <Footer/>
+           
             </div>
-        </>
+      
     );
+
+
+
+
+    return (
+      
+        <div classname={styles.container}>
+            <Header />
+                <Band {...band}/> 
+            <Link to={`/`}>
+              <button>Voltar</button></Link>
+              
+            <Aside/>
+            <Footer/>
+       
+        </div>
+
+
+
+
+
+
+
+    );
+
+
 }
 
 export default BandDetalhes;
